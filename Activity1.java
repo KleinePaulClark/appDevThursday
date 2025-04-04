96165c310f312486de85650a8e2f5b87669c3300
KleinePaulClark <kclark@paterostechnologicalcollege.edu.ph>
2025-04-04 18:08:22 +0800

    Added name: Jay-ar D. Melendres

---
+++ Activity1.java
@@ -0,0 +1,9 @@
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+        System.out.println("Jay-ar D. Melendres - BSIT-2L")
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name - SECTION");
+    }
+}
